7ced650a0c4edb7405c5719384b97fabcf92abd6
samueltribulo <samueltribulo@gmail.com>
2021-11-24 13:23:57 -0300

    ultimo de los ultimos

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -126,7 +126,8 @@ function esEntero(numero) {
   // Tu código:
   if (numero - Math.floor(numero) === 0){
     return "true";
-  }else{
+  }
+  else{
     return "false";
   }
 }
@@ -197,7 +198,12 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
-
+  if (valor == true){
+    return "Soy verdadero";
+  }
+  else if(valor == false){
+    return "Soy falso";
+  }
 
 }
 
@@ -205,19 +211,35 @@ function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
   //Escribe tu código aquí   
-  
+  var tablaDel6 = [];
+  for (var i=0; i<=10; i++){
+    tablaDel6.push(i*6);
+  }
+  return tablaDel6;
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+  if (numero < 100 || numero > 999 || numero < -99 || numero < -999){
+    return false;
+  }
+  else {
+    return true;
+  }
 }
 
 function doWhile(numero) {
   //Implementar una función tal que vaya aumentando el valor recibido en 5 hasta un límite de 8 veces
   //Retornar el valor final.
   //Usar el bucle do ... while.
+  var i = 0;
+  do {
+    i++;
+    numero+=5;
+  }
+  while (i<8);
+  return numero;
 }
 
 
