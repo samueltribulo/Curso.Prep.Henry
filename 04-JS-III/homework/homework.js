4e666e8a1758f5a260104f95f081f56dd8b04091
samueltribulo <samueltribulo@gmail.com>
2021-11-26 15:00:46 -0300

    Samu

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -115,6 +115,14 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if(arguments.length<1){
+    return 0;
+  }
+  var producto = arguments[0];
+  for (var i = 1; i < arguments.length; i++){
+    producto = producto * arguments[i];
+  }
+  return producto;
 }
 
 
